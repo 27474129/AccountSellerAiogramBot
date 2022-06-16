98b2b740ff8f0f7a9ff62f89c905b31a042263ee
Vladimir <harlanvova03@gmail.com>
2022-06-16 16:12:59 +0700

    asd

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    BOT_TOKEN = ""
+    ADMINS_ID = []
+    TEST_PAYMENT_TOKEN = ""
+    PAYMENT_TOKEN = "#"
+    hostname = "127.0.0.1"
+    pg_username = "postgres"
+    pg_password = ""
+    pg_db_name = ""
+    pg_port_id = int()
